a0280e02f46351077d9702198a946b429a8a27d2
Gui23x <107221655+gui23x@users.noreply.github.com>
2022-07-01 21:25:46 -0300

    Add files via upload

---
+++ Lab_12/Lab_13/View/Main.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Lab_13.View
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnLer = new System.Windows.Forms.Button();
+            this.lbxResultado = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI Semibold", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.lblTitulo.Location = new System.Drawing.Point(33, 48);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(382, 32);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Leitor de informações delimitadas";
+            // 
+            // btnLer
+            // 
+            this.btnLer.Location = new System.Drawing.Point(144, 105);
+            this.btnLer.Name = "btnLer";
+            this.btnLer.Size = new System.Drawing.Size(149, 45);
+            this.btnLer.TabIndex = 1;
+            this.btnLer.Text = "Ler";
+            this.btnLer.UseVisualStyleBackColor = true;
+            this.btnLer.Click += new System.EventHandler(this.btnLer_Click);
+            // 
+            // lbxResultado
+            // 
+            this.lbxResultado.FormattingEnabled = true;
+            this.lbxResultado.ItemHeight = 15;
+            this.lbxResultado.Location = new System.Drawing.Point(12, 179);
+            this.lbxResultado.Name = "lbxResultado";
+            this.lbxResultado.Size = new System.Drawing.Size(427, 259);
+            this.lbxResultado.TabIndex = 2;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 450);
+            this.Controls.Add(this.lbxResultado);
+            this.Controls.Add(this.btnLer);
+            this.Controls.Add(this.lblTitulo);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Leitor de Texto";
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnLer;
+        private System.Windows.Forms.ListBox lbxResultado;
+    }
+}
---
+++ Lab_12/Lab_13/View/Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab_13.View
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLer_Click(object sender, EventArgs e)
+        {
+            string endereco = @"D:\senac\teste007.txt";
+
+            try
+            {
+                if (!System.IO.File.Exists(endereco))
+                {
+                    throw new Exception(
+                        "Arquivo teste007.txt não foi localizado."
+                        );
+                }
+
+                string[] dados = new string[4];
+
+                System.IO.StreamReader Leitor = new System.IO.StreamReader(endereco);
+
+                lbxResultado.Items.Add(
+                    "Código".PadRight(20) +
+                    "Cliente".PadRight(27) +
+                    "Cidade".PadRight(30) +
+                    "País"
+                    );
+                lbxResultado.Items.Add(new string('-', 80));
+
+                while (!Leitor.EndOfStream)
+                {
+                    dados = Leitor.ReadLine().Split(';');
+
+                    lbxResultado.Items.Add(
+                        dados[0].PadRight(25) +
+                        dados[1].PadRight(27) +
+                        dados[2].PadRight(28) +
+                        dados[3]);
+                }
+
+                Leitor.Close();
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Alerta de Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                
+            }
+        }
+    }
+}
